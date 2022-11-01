cfc4ad0f683d329c7dd973cbb2f2c2b22c4fe8f3
zhengyang yang <102400789+awsxr168@users.noreply.github.com>
2022-10-31 23:39:05 -0700

    Update and rename flood_fill_starter.py to flood_fill.py

--- wk4submission/flood_fill.py
+++ wk4submission/flood_fill.py
@@ -26,6 +26,26 @@ def flood_fill(input_board: List[str], old: str, new: str, x: int, y: int) -> Li
     """
 
     # Implement your code here.
+    if input_board[x][y] != old or x < 0 or y < 0 or x > len(input_board[y])-1 or y > len(input_board[x])-1:
+        return
+    
+    input_board[x][y] == new    
+    flood_fill(input_board, old, new, x-1, y)
+    flood_fill(input_board, old, new, x+1, y)
+    flood_fill(input_board, old, new, x, y-1)
+    flood_fill(input_board, old, new, x, y+1)
 
-print(board)
+modified_board = flood_fill(input_board=board, old=".", new="~", x=5, y=12)
 
+for a in modified_board:
+    print(a)
+
+# Expected output:
+# ......................
+# ......##########......
+# ......#~~~~~~~~#......
+# ......#~~~~~~~~#......
+# ......#~~~~~~~~#####..
+# ....###~~~~~~~~~~~~#..
+# ....#~~~~~~~~~~~~###..
+# ....##############....
