e4025fa4773680a2bf9f8c6a79cd15a1a640ba9d
zhengyang yang <102400789+awsxr168@users.noreply.github.com>
2022-10-31 23:17:26 -0700

    Update and rename foods.txt to flood-fill.py

---
+++ flood-fill.py
@@ -0,0 +1,51 @@
+from typing import List
+
+board = [
+    "......................",
+    "......##########......",
+    "......#........#......",
+    "......#........#......",
+    "......#........#####..",
+    "....###............#..",
+    "....#............###..",
+    "....##############....",
+]
+
+def flood_fill(input_board: List[str], old: str, new: str, x: int, y: int) -> List[str]:
+    """Returns board with old values replaced with new values
+    through flood filling starting from the coordinates x, y
+    Args:
+        input_board (List[str])
+        old (str): Value to be replaced
+        new (str): Value that replaces the old
+        x (int): X-coordinate of the flood start point
+        y (int): Y-coordinate of the flood start point
+    Returns:
+        List[str]: Modified board
+    """
+
+    # Implement your code here.
+
+    if input_board[x][y] == "#" or x < 0 or y < 0 or x > len(input_board[y])-1 or y > len(input_board[x])-1:
+        return
+    
+    input_board[x][y] == new    
+    flood_fill(input_board, old, new, x-1, y)
+    flood_fill(input_board, old, new, x+1, y)
+    flood_fill(input_board, old, new, x, y-1)
+    flood_fill(input_board, old, new, x, y+1)
+    
+modified_board = flood_fill(input_board=board, old=".", new="~", x=5, y=12)
+
+for a in modified_board:
+    print(a)
+
+# Expected output:
+# ......................
+# ......##########......
+# ......#~~~~~~~~#......
+# ......#~~~~~~~~#......
+# ......#~~~~~~~~#####..
+# ....###~~~~~~~~~~~~#..
+# ....#~~~~~~~~~~~~###..
+# ....##############....
